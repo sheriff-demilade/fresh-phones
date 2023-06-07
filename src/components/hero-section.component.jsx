8db50bf8ddfbd8f1459c6d344de0c3249f4854c2
sheriff-demilade <sheriffdemilade@gmail.com>
2023-06-07 22:49:54 +0100

    created hero-section component and styles the header component

---
+++ src/components/hero-section.component.jsx
@@ -0,0 +1,41 @@
+const HeroSection = () => {
+  return (
+    <section className="pt-16 pb-20 bg-blue-50">
+      <div className="container mx-auto px-8 grid grid-cols-2 gap-24 items-center">
+        <div className="text-gray-700">
+          <h2 className="text-4xl font-medium mb-6">
+            Lorem ipsum dolor sit amet bonjour elit.
+          </h2>
+          <p className="text-lg mb-12">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
+            laboriosam minus labore repellat similique dicta reiciendis
+            repudiandae autem. Voluptas adipisci eligendi veritatis facilis, non
+            iure velit doloribus nesciunt in dignissimos.
+          </p>
+
+          <form
+            action=""
+            className="flex items-center w-2/3 shadow rounded-2xl"
+          >
+            <input
+              type="search"
+              className="h-10 grow rounded-l-2xl px-6 outline-none"
+            />
+            <button className="h-10 bg-blue-400 rounded-r-2xl text-white text-xl px-2">
+              <ion-icon name="search-outline"></ion-icon>
+            </button>
+          </form>
+        </div>
+        <div className="">
+          <img
+            src="https://plus.unsplash.com/premium_photo-1682065724607-22877c1b6983?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjV8fG1vYmlsZSUyMHBob25lc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=400&q=60"
+            alt="Mobile phone"
+            className="w-4/5 rounded-3xl mx-auto"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
--- src/routes/header.component.jsx
+++ src/routes/header.component.jsx
@@ -4,24 +4,77 @@ import { Link, Outlet } from "react-router-dom";
 const Header = () => {
   return (
     <Fragment>
-      <header className="flex justify-between items-center h-16 px-8 shadow">
-        <Link to="/" className="text-lg">
-          LOGO
+      <header className="flex justify-between gap-20 items-center h-16 px-8 bg-blue-50 border border-blue-100">
+        <Link to="/" className="text-3xl  text-blue-300 logo">
+          FreshPhones
         </Link>
 
-        <nav className="main-nav">
-          <ul className="flex gap-8 text-lg">
+        <nav className="">
+          <ul className="flex items-center gap-8 tracking-wider text-xs font-semibold text-gray-500 ">
             <li>
-              <Link to="/shop">SHOP</Link>
+              <Link
+                to="/"
+                className="hover:text-blue-400 transition duration-300"
+              >
+                HOME
+              </Link>
             </li>
             <li>
-              <Link to="/checkout">CHECKOUT</Link>
+              <Link
+                to="/shop"
+                className="hover:text-blue-400 transition duration-300"
+              >
+                SHOP
+              </Link>
             </li>
             <li>
-              <Link to="/login">LOG IN</Link>
+              <Link
+                to="/"
+                className="hover:text-blue-400 transition duration-300"
+              >
+                ABOUT US
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/"
+                className="hover:text-blue-400 transition duration-300"
+              >
+                CONTACT US
+              </Link>
             </li>
           </ul>
         </nav>
+
+        <ul className="flex items-center  gap-8 tracking-wider text-xs font-semibold text-gray-500 ">
+          <li>
+            <Link
+              to="/checkout"
+              className="hover:text-blue-400 transition duration-300 inline-block relative"
+            >
+              <ion-icon name="cart-outline" size="large"></ion-icon>
+              <div className="w-4 h-4 rounded-full absolute top-0 right-0 translate-x-1/4 -translate-y-1/5 bg-blue-400 text-white text-xs text-center">
+                3
+              </div>
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/"
+              className="hover:text-blue-400 transition duration-300"
+            >
+              LOG IN
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/"
+              className="px-4 py-2 bg-blue-400 text-white rounded-md hover:text-blue-400  hover:bg-white transition duration-300 hover:outline outline-1 outline-blue-400"
+            >
+              SIGN UP
+            </Link>
+          </li>
+        </ul>
       </header>
 
       <Outlet />
